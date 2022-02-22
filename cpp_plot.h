e593d32628cab43d6a96bce599e9e8328cb734b2
Harrison Hunter <harrisonhunter27@gmail.com>
2022-02-22 08:53:00 -0600

    Added scatter plot labels and font size for title and label font

--- cpp_plot.h
+++ cpp_plot.h
@@ -18,20 +18,25 @@ class GNUPlot {
                   int type=0,
                   int weight=2);
 
-        template<class ContainerX, class ContainerY>
+        template<class ContainerX, class ContainerY, class ContainerZ>
         void scatter(ContainerX const& x, ContainerY const& y,
                 std::string name="",
+                ContainerZ const* labels=nullptr,
+                std::string label_offset="1,1",
                 std::string style="");
 
         inline void setTitle(std::string title) { this->title = title; };
         inline void setXLabel(std::string xlabel) { this->xlabel = xlabel; };
         inline void setYLabel(std::string ylabel) { this->ylabel = ylabel; };
+        inline void setLabelFont(int size) { this->label_font = size; };
+        inline void setTitleFont(int size) { this->title_font = size; };
 
         void show();
 
     private:
         std::string title;
         std::string xlabel, ylabel;
+        int label_font = 0, title_font = 0;
 
         std::stringstream plotting_data;
         std::stringstream plotting_cmds;
@@ -40,6 +45,8 @@ class GNUPlot {
 
         int data_ind;
 
+        template<class ContainerX, class ContainerY, class ContainerZ>
+        void add_points(ContainerX const& x, ContainerY const& y, ContainerZ const* z);
         template<class ContainerX, class ContainerY>
         void add_points(ContainerX const& x, ContainerY const& y);
         void run_gnuplot();
@@ -78,17 +85,27 @@ void GNUPlot::plot(ContainerX const& x, ContainerY const& y,
 }
 
 /// Draw a scatter plot
-template<class ContainerX, class ContainerY>
+template<class ContainerX, class ContainerY, class ContainerZ=std::nullptr_t>
 void GNUPlot::scatter(ContainerX const& x, ContainerY const& y,
         std::string name,
+        ContainerZ const* labels,
+        std::string label_offset,
         std::string style)
 {
     // Add points
-    add_points(x, y);
+    if (labels != nullptr) {
+        add_points(x, y, labels);
+    } else {
+        add_points(x, y);
+    }
 
     // Add style
     if (data_ind > 1) plotting_cmds << ", ";
-    plotting_cmds << "$data" << data_ind << " with points";
+    plotting_cmds << "$data" << data_ind;
+    if (labels)
+        plotting_cmds << " with labels point offset char " <<  label_offset;
+    else
+        plotting_cmds << " with points";
 
     // Add name
     if (!name.empty())
@@ -108,6 +125,24 @@ void GNUPlot::show()
 }
 
 /// Add datapoints under the variable assigned by data_ind
+template<class ContainerX, class ContainerY, class ContainerZ>
+void GNUPlot::add_points(ContainerX const& x, ContainerY const& y, ContainerZ const* labels)
+{
+    data_ind++;
+    plotting_data << "\n$data" << data_ind << " << EOD\n";
+
+    auto x_iter = x.begin();
+    auto y_iter = y.begin();
+    auto label_iter = labels->begin();
+    while (x_iter != x.end() || y_iter != y.end()) {
+        plotting_data << *x_iter << " " << *y_iter << " " << *label_iter <<  "\n";
+        x_iter++;
+        y_iter++;
+        label_iter++;
+    }
+    plotting_data << "EOD\n\n";
+}
+
 template<class ContainerX, class ContainerY>
 void GNUPlot::add_points(ContainerX const& x, ContainerY const& y)
 {
@@ -117,7 +152,7 @@ void GNUPlot::add_points(ContainerX const& x, ContainerY const& y)
     auto x_iter = x.begin();
     auto y_iter = y.begin();
     while (x_iter != x.end() || y_iter != y.end()) {
-        plotting_data << *x_iter << " " << *y_iter << "\n";
+        plotting_data << *x_iter << " " << *y_iter <<  "\n";
         x_iter++;
         y_iter++;
     }
@@ -128,9 +163,12 @@ std::string GNUPlot::generate_script()
 {
     std::stringstream script_strm;
 
-    script_strm << "\nset title \"" << title << "\"\n";
-    script_strm << "set xlabel \"" << xlabel << "\"\n";
-    script_strm << "set ylabel \"" << ylabel << "\"\n";
+    script_strm << "\nset title \"" << title << "\"";
+    script_strm << (title_font ? " font \"," + std::to_string(title_font) + "\"\n" : "\n");
+    script_strm << "set xlabel \"" << xlabel << "\"";
+    script_strm << (label_font ? " font \"," + std::to_string(label_font) + "\"\n" : "\n");
+    script_strm << "set ylabel \"" << ylabel << "\"";
+    script_strm << (label_font ? " font \"," + std::to_string(label_font) + "\"\n" : "\n");
     script_strm << "set autoscale\n";
     script_strm << plotting_data.str() << "\n";
     script_strm << "plot " << plotting_cmds.str() << "\n";
--- example.cpp
+++ example.cpp
@@ -9,19 +9,24 @@ int main() {
 
     std::vector<float> x;
     std::vector<float> y;
+    std::vector<int> labels;
     for (int i = 0; i < 10; i++) {
         float t = static_cast<float>(i);
         x.push_back(t);
         y.push_back(sin(M_PI * (t / 10.0)));
+        labels.push_back(i);
     }
 
     plt.plot(x, y, "Sin()");
-    plt.scatter(y, x, "Scatter");
+    plt.scatter(y, x, "Scatter", &labels);
 
     for (int i = 0; i < 10; i++) {
         y[i] += 1;
     }
     plt.plot(x, y, "Offset", true, "#B0A510", 0, 5);
+
+    plt.setLabelFont(12);
+    plt.setTitleFont(15);
 
     plt.show();
     return 0;
